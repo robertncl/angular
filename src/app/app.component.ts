1fa9d70f9557646a3a451909b9555d95f8ebb48f
Robert Ngo <robertncl@gmail.com>
2026-01-11 19:19:34 +0800

    Remove unused RouterOutlet import from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { WeatherComponent } from './weather/weather.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, WeatherComponent],
+  imports: [WeatherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
